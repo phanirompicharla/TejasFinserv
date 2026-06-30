0700f128a5c4e05272a535255beefde0ac1c7a29
GURUCHARAN TANNEERU <guru.charan.tanneeru@gmail.com>
2026-06-30 23:34:33 +0530

    Fix typescript unused import error in AdminDashboard

--- src/pages/admin/AdminDashboard.tsx
+++ src/pages/admin/AdminDashboard.tsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
 import { Button } from '../../components/Button';
 import { Logo } from '../../components/Logo';
 
