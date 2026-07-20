526fc096962ace15dd5df0cd9a155ed9f48259f7
Balu <balu.kodeboyina@gmail.com>
2026-07-20 22:21:01 +0530

    Style: make footer typography much more prominent (sizes and weights)

--- src/components/calculators/TakeSnapshot.tsx
+++ src/components/calculators/TakeSnapshot.tsx
@@ -122,29 +122,26 @@ export function TakeSnapshot({ title, inputs, resultsNode, filename }: TakeSnaps
         ctx.setTransform(1, 0, 0, 1, 0, 0)
 
         const scale = 2
-        
-        // Match the 64px (px-16) padding of the layout container
-        const horizontalMargin = 64 * scale
-        
-        // 45px padding from the bottom inside the cream area
+        const horizontalMargin = 65 * scale
         const bottomPadding = 45 * scale
         
-        const fontSize = 16 * scale
-        const lineHeight = 24 * scale
-
-        ctx.fillStyle = '#1e293b' // dark navy text
-        ctx.font = `500 ${fontSize}px sans-serif` // Medium weight, elegant font
-        ctx.textBaseline = 'bottom'
-
         const yPos = canvas.height - bottomPadding
 
+        ctx.fillStyle = '#0B1526' // dark navy color
+        ctx.textBaseline = 'bottom'
+
         // Bottom-left
+        const leftFontSize = 28 * scale
+        ctx.font = `700 ${leftFontSize}px sans-serif`
         ctx.textAlign = 'left'
         ctx.fillText('Powered by TejasFinserv', horizontalMargin, yPos)
 
         // Bottom-right
+        const rightFontSize = 24 * scale
+        const rightLineHeight = rightFontSize * 1.5 // 1.5 line spacing
+        ctx.font = `600 ${rightFontSize}px sans-serif`
         ctx.textAlign = 'right'
-        ctx.fillText('ARN-251896', canvas.width - horizontalMargin, yPos - lineHeight)
+        ctx.fillText('ARN-251896', canvas.width - horizontalMargin, yPos - rightLineHeight)
         ctx.fillText('9490716662', canvas.width - horizontalMargin, yPos)
 
         ctx.restore()
